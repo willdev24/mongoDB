9839a107f8d71099f02d3993137817c4073d1a7b
willy thiago <128657856+willdev24@users.noreply.github.com>
2024-05-28 21:22:27 -0400

    k

---
+++ src/routes/index.js
@@ -0,0 +1,24 @@
+const router = require("express").Router()
+
+router.get("/", (req,res)=>{
+
+    res.render("index")
+
+})
+
+
+router.get("/api",(req, res)=>{
+    
+    res.sendFile(__dirname +  '/cadastrados.json')
+  
+})
+
+router.use((req,res)=>{
+
+    res.send("erro, pagna nao encontrada")
+
+})
+
+module.exports  = {
+    router}
+
